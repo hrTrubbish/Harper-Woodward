add23152bb834c8dd4011a747ae72bda43c29ceb
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-03-04 19:45:04 -0800

    Add skeleton to VideoPlayer and AllVideos based on wireframe

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,19 +4,24 @@ import Home from './pages/components/_Home';
 import Header from './pages/components/common/Header.jsx';
 // import { AuthEmail } from './pages/components/_AuthEmail';
 // import { AuthGoogle } from './pages/components/_AuthGoogle';
-import Live from './pages/components/_Live';
+import Live from './pages/LivePage';
+import AllVideos from './pages/AllVideos';
+import VideoPlayer from './pages/VideoPlayer';
 
 const App = () => {
   return (
     <>
       <Header />
       <div className="App">
+        {/* <VideoPlayer */}
         {/* <AuthEmail />
         <AuthGoogle /> */}
-        {/* <Routes>
+        <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/live" element={<Live />} />
-        </Routes> */}
+          <Route path="/videos" element={<AllVideos />} />
+          <Route path="/video-player" element={<VideoPlayer />} />
+        </Routes>
       </div>
     </>
   );
--- src/client/pages/VideoPlayer.jsx
+++ src/client/pages/VideoPlayer.jsx
@@ -2,19 +2,28 @@ import React from 'react';
 
 const VideoPlayer = () => {
   return (
-    <div>
-      <h1>Brooks Garth</h1>
-      <div>Title1
-        <video></video>
+    <div className="flex">
+      <div className="flex w-2/3 border-solid border-2 border-inherit p-6">
+        <div className="flex flex-col w-full border-solid border-2 border-rose-100 gap-6">
+          <video className="border-solid border-2 border-rose-100"></video>
+          <h2>Dancing in a Bar</h2>
+          <p>{`Views: ${125000} Likes: ${1000}`}</p>
+        </div>
       </div>
-      <div>Title2
-        <video></video>
-      </div>
-      <div>Title3
-        <video></video>
-      </div>
-      <div>Title4
-        <video></video>
+      <div className="flex flex-col w-1/3 border-solid border-2 border-inherit p-6 gap-6">
+        <div className="w-1/2 self-center border-solid border-2 border-rose-100">
+          <img className="h-24"></img>
+        </div>
+        <div className="w-1/2 self-center border-solid border-2 border-rose-100">
+          <img className="h-24"></img>
+        </div>
+        <div className="w-1/2 self-center border-solid border-2 border-rose-100">
+          <img className="h-24"></img>
+        </div>
+        <div className="w-1/2 self-center border-solid border-2 border-rose-100">
+          <img className="h-24"></img>
+        </div>
+        <button>More</button>
       </div>
     </div>
   );
