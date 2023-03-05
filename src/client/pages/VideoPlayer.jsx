8088512bb3843f718406969b036fed07b9112e6b
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-03-04 18:23:01 -0800

    VideoPlayer start

---
+++ src/client/pages/VideoPlayer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const VideoPlayer = () => {
+  return (
+    <div>
+      <h1>Brooks Garth</h1>
+      <div>Title1
+        <video></video>
+      </div>
+      <div>Title2
+        <video></video>
+      </div>
+      <div>Title3
+        <video></video>
+      </div>
+      <div>Title4
+        <video></video>
+      </div>
+    </div>
+  );
+};
+
+export default VideoPlayer;
