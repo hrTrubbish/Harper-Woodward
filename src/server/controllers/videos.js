eba1d428addab13446df19cd3d35e0af16686204
Kevin Le <iamkevinvle@gmail.com>
2023-03-06 15:14:27 -0800

    finish get/post videos route

--- src/server/controllers/videos.js
+++ src/server/controllers/videos.js
@@ -1,1 +1,35 @@
-module.exports = {};
+const model = require('../models/videos');
+
+module.exports = {
+  get: async (req, res) => {
+    try {
+      const videos = await model.getAllVideos();
+      res.status(200).send(videos);
+    } catch (err) {
+      console.log(err, 'err');
+    }
+  },
+  post: async (req, res) => {
+    try {
+      console.log('in videos addVideo')
+      model.addVideo(req.body);
+      res.status(201).send('in addVideos')
+    } catch (err) {
+      console.log(err, 'err');
+    }
+  },
+  patch: async (req, res) => {
+    try {
+      console.log('in videos updateVideo')
+    } catch (err) {
+      console.log(err, 'err');
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      console.log('in videos deleteVideo')
+    } catch (err) {
+      console.log(err, 'err');
+    }
+  },
+};
