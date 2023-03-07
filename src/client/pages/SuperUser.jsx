e33c6e33ca27a6ce4d4260073a444a40aff5e261
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-03-06 16:30:36 -0800

    Fix SuperUser borders

--- src/client/pages/SuperUser.jsx
+++ src/client/pages/SuperUser.jsx
@@ -4,8 +4,8 @@ import StatsList from './components/super_user/StatsList.jsx';
 export default function SuperUser() {
   return (
     <div className="flex">
-      <div className="flex flex-col w-1/2 ml-8 mr-8 mt-8 border-solid border-2 border-black">
-        <button type="button" className="text-3xl">Start Livestream</button>
+      <div className="flex flex-col w-1/2 m-8 border-solid border-2 border-current">
+        <button type="button" className="flex-1 text-3xl">Start Livestream</button>
       </div>
       <StatsList />
     </div>
