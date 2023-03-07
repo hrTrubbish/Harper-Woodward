3578b4bdfeebd85398e30963379ada53f6f93134
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-03-07 00:04:03 -0800

    Add hover color change to button

--- src/client/pages/SuperUser.jsx
+++ src/client/pages/SuperUser.jsx
@@ -6,7 +6,7 @@ export default function SuperUser() {
     <div className="h-screen">
       <div className="flex">
         <div className="flex justify-center w-1/2">
-          <button type="button" className="bg-garthbeige hover:bg-brown-200 text-garthbrown font-bold py-2 px-4 rounded p-6 m-8 self-center">Start Livestream</button>
+          <button type="button" className="bg-garthbeige hover:bg-white text-garthbrown font-bold py-2 px-4 rounded p-6 m-8 self-center">Start Livestream</button>
         </div>
         <StatsList />
       </div>
