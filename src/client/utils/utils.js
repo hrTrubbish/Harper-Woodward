85695d86d0640bba9d82bce3c7ae4f95b8cf9e4b
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-03-07 14:55:55 -0800

    Research forms and requests pair programming

---
+++ src/client/utils/utils.js
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon';
+
+
+const convertToUTC = (timeInput) => {
+
+  const timeValue = timeInput.value;
+
+  const today = DateTime.local();
+  const selectedTime = DateTime.fromFormat(timeValue, 'HH:mm:ss');
+
+  const combined = today.set({
+    hour: selectedTime.hour,
+    minute: selectedTime.minute,
+    second: selectedTime.second,
+    millisecond: selectedTime.millisecond
+  }).toUTC();
+
+  const timestamp = combined.toMillis();
+  return timestamp
+}
+
+export {convertToUTC}
