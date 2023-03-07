1fa5028dd7bc06540d8f702c8fbabc5c35e67465
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-03-06 20:33:09 -0800

    More clean up

--- src/client/pages/components/super_user/StatsList.jsx
+++ src/client/pages/components/super_user/StatsList.jsx
@@ -9,13 +9,10 @@ export default function StatsList({ stats }) {
         {/* {stats.map((stat) => {
           return <ul><StatsItem key={stat._id} id={stat._id} stat={stat}/></ul>
         })} */}
-        <StatsItem className="border-solid border-2 border-current">stats 1</StatsItem>
-        <StatsItem className="border-solid border-2 border-current">stats 2</StatsItem>
-        <StatsItem className="border-solid border-2 border-current">stats 3</StatsItem>
-        <StatsItem className="border-solid border-2 border-current">stats 4</StatsItem>
-        <StatsItem className="border-solid border-2 border-current">stats 5</StatsItem>
-        <StatsItem className="border-solid border-2 border-current">stats 6</StatsItem>
-        <StatsItem className="border-solid border-2 border-current">stats 7</StatsItem>
+        <StatsItem />
+        <StatsItem />
+        <StatsItem />
+        <StatsItem />
       </div>
     </div>
   );
