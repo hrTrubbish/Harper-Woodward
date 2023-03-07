e6eefa78cfbd5939bc1934daea2a1b4e3219c7a6
David Zhang <david.zny1022@gmail.com>
2023-03-06 22:19:46 -0800

    all routes

--- src/server/models/admin.js
+++ src/server/models/admin.js
@@ -1,5 +1,63 @@
+const {
+  collection,
+  getDoc,
+  getDocs,
+  doc,
+  setDoc,
+  updateDoc,
+} = require('firebase/firestore');
+const dbPromise = require('../../config/firebase');
+
 module.exports = {
-  get: async () => {},
-  post: async () => {},
-  patch: async () => {},
+  get: async () => {
+    try {
+      const db = await dbPromise;
+      const superUserRef = collection(db, 'superUsers');
+      const superUserSnapShot = await getDocs(superUserRef);
+      const superUsers = superUserSnapShot.docs.map(
+        (d) => ({
+          id: d.id,
+          ...d.data(),
+        }),
+      );
+
+      return superUsers;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+  post: async (payload) => {
+    try {
+      const db = await dbPromise;
+      const superUsersRef = collection(db, 'superUsers');
+
+      const newsuperUserRef = await setDoc(
+        doc(superUsersRef),
+        payload,
+      );
+
+      const newsuperUser = await getDoc(newsuperUserRef);
+      return {
+        id: newsuperUser.id,
+        ...newsuperUser.data(),
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+  patch: async (id, payload) => {
+    try {
+      const db = await dbPromise;
+      const superUserRef = doc(db, 'superUsers', id);
+      await updateDoc(superUserRef, payload);
+
+      const updatedsuperUser = await getDoc(superUserRef);
+      return {
+        id: updatedsuperUser.id,
+        ...updatedsuperUser.data(),
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
 };
--- src/server/models/transactions.js
+++ src/server/models/transactions.js
@@ -1,4 +1,54 @@
+const {
+  collection,
+  getDoc,
+  getDocs,
+  doc,
+  setDoc,
+} = require('firebase/firestore');
+const dbPromise = require('../../config/firebase');
+
 module.exports = {
-  get: async () => {},
-  post: async () => {},
+  get: async () => {
+    try {
+      const db = await dbPromise;
+      const transactionRef = collection(db, 'transactions');
+      const transactionSnapShot = await getDocs(
+        transactionRef,
+      );
+      const transactions = transactionSnapShot.docs.map(
+        (d) => ({
+          id: d.id,
+          ...d.data(),
+        }),
+      );
+
+      return transactions;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+  post: async (payload) => {
+    try {
+      const db = await dbPromise;
+      const transactionsRef = collection(
+        db,
+        'transactions',
+      );
+
+      const newtransactionRef = await setDoc(
+        doc(transactionsRef),
+        payload,
+      );
+
+      const newtransaction = await getDoc(
+        newtransactionRef,
+      );
+      return {
+        id: newtransaction.id,
+        ...newtransaction.data(),
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
 };
