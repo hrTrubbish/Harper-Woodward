9924048d64b576f17650d9ed36123619b63b3a5e
George Khoury <gke@sent.com>
2023-03-07 16:37:53 -0800

    adding sidebar nav to admin page

--- src/client/pages/SuperUser.jsx
+++ src/client/pages/SuperUser.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import StatsList from './components/super_user/StatsList.jsx';
 import ScheduleStreams from './components/super_user/ScheduleStreams.jsx';
 import AddTourDates from './components/super_user/AddTourDates.jsx';
+import NavigationDrawer from './components/super_user/NavigationDrawer.jsx';
 
 export default function SuperUser() {
   return (
-    <div className="h-screen">
-      <div className="flex">
-        <div className="flex justify-center w-1/2">
-          <button type="button" className="bg-garthbeige hover:bg-white text-garthbrown font-bold py-2 px-4 rounded p-6 m-8 self-center">Start Livestream</button>
-        </div>
-        <StatsList />
+    <div className="h-screen flex">
+      <div className="w-1/4 max-w-xs">
+        <NavigationDrawer />
       </div>
-      <div>
-        <AddTourDates />
+      <div className="w-3/4">
+        <div className="flex">
+          <div className="flex justify-center w-1/2">
+            <button type="button" className="bg-garthbeige hover:bg-white text-garthbrown font-bold py-2 px-4 rounded p-6 m-8 self-center">Start Livestream</button>
+          </div>
+          <StatsList />
+        </div>
+        <div>
+          <AddTourDates />
+        </div>
       </div>
     </div>
   );
---
+++ src/client/pages/components/super_user/NavigationDrawer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NavigationDrawer() {
+  return (
+    <div className="flex flex-col">
+      <div>test</div>
+      <div>word</div>
+    </div>
+  );
+}
+
+export default NavigationDrawer;
