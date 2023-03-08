09c85d7030ad919118e77cf764215f2f4a53f9a9
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-03-08 12:25:28 -0800

    Add Merch component with photos, basic setup

--- src/client/pages/Home.jsx
+++ src/client/pages/Home.jsx
@@ -5,6 +5,7 @@ import YouTubeEmbed from './components/video_player/YouTubeEmbed.jsx';
 import {
   TourDateList,
   StreamInfo,
+  Merch,
 } from './components/home';
 
 export default function Home() {
@@ -68,26 +69,8 @@ export default function Home() {
         <span className="text-3xl self-center">tours</span>
         <TourDateList tours={tours} />
       </div>
-      <div className="merch flex flex-col p-10 border-t-2 border-current">
-        <span className="text-3xl self-center">merch</span>
-        <div className="flex justify-around mb-20 p-5">
-          <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
-            <span className="text-sm self-center">Books</span>
-            <img src="src/client/styles/img/BGBook.jpg" alt="" />
-          </div>
-          <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
-            <span className="text-sm self-center">Masks</span>
-            <img src="src/client/styles/img/BGFacemask.jpg" alt="" />
-          </div>
-          <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
-            <span className="text-sm self-center">Albums</span>
-            <img src="src/client/styles/img/BGRecord.jpg" alt="" />
-          </div>
-          <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
-            <span className="text-sm self-center">Clothes</span>
-            <img src="src/client/styles/img/BGSweatshirt.jpg" alt="" />
-          </div>
-        </div>
+      <div>
+        <Merch />
       </div>
     </div>
   );
