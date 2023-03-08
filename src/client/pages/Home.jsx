87a0da2d698a46567c9453282bd0833173c497d5
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-03-08 11:57:36 -0800

    Home Merch CSS

--- src/client/pages/Home.jsx
+++ src/client/pages/Home.jsx
@@ -67,26 +67,23 @@ export default function Home() {
       </div>
       <div className="merch flex flex-col p-10 border-t-2 border-current">
         <span className="text-3xl self-center">merch</span>
-        {/* <div className="sm:flex sm:items-center sm:justify-between"> */}
-          <div className="flex justify-around mb-20 p-5">
-            <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
-              <span className="text-sm self-center">Books</span>
-              <img src="src/client/styles/img/BGBook.jpg" alt="" />
-            </div>
-            <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
-              <span className="text-sm self-center">Masks</span>
-              <img src="src/client/styles/img/BGFacemask.jpg" alt="" />
-            </div>
-            <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
-              <span className="text-sm self-center">Albums</span>
-              <img src="src/client/styles/img/BGRecord.jpg" alt="" />
-            </div>
-            <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
-              <span className="text-sm self-center">Clothes</span>
-              <img src="src/client/styles/img/BGSweatshirt.jpg" alt="" />
-            </div>
-          {/* </div> */}
-
+        <div className="flex justify-around mb-20 p-5">
+          <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
+            <span className="text-sm self-center">Books</span>
+            <img src="src/client/styles/img/BGBook.jpg" alt="" />
+          </div>
+          <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
+            <span className="text-sm self-center">Masks</span>
+            <img src="src/client/styles/img/BGFacemask.jpg" alt="" />
+          </div>
+          <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
+            <span className="text-sm self-center">Albums</span>
+            <img src="src/client/styles/img/BGRecord.jpg" alt="" />
+          </div>
+          <div className="w-40 cursor-pointer hover:scale-125 transition-transform border-solid">
+            <span className="text-sm self-center">Clothes</span>
+            <img src="src/client/styles/img/BGSweatshirt.jpg" alt="" />
+          </div>
         </div>
       </div>
     </div>
