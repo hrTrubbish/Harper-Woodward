596014b230f7a205db482e0a5dc82be31f40e44d
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-03-09 13:00:22 -0800

    AllVideos css to add outline to video titles. Add hover. Add content from youtube and feature video import capabilities.

--- src/client/pages/AllVideos.jsx
+++ src/client/pages/AllVideos.jsx
@@ -20,39 +20,39 @@ export default function AllVideos() {
     <div className="p-6 w-screen">
       <div className="max-w-7xl m-auto">
         <div className="grid gap-24 grid-cols-2 grid-rows-auto">
-          <div className="border-solid border-2 border-current">
+          <div className="hover:scale-105 transition-transform">
             <YouTubeEmbed
               src={featuredVideo[0]?.url}
               title="*FEATURE VIDEO*"
             />
-            <div className="border-solid border-t-2 border-current p-2 text-center">
+            <div className="p-2 text-2xl text-center font-outline-2">
               <h2>*FEATURE VIDEO*</h2>
             </div>
           </div>
-          <div className="border-solid border-2 border-current">
+          <div className="hover:scale-105 transition-transform">
             <YouTubeEmbed
               src="https://www.youtube.com/embed/mvCgSqPZ4EM"
               title="Friends in Low Places"
             />
-            <div className="border-solid border-t-2 border-current p-2 text-center">
+            <div className="p-2 text-xl text-center font-outline-2">
               <h2>Friends in Low Places</h2>
             </div>
           </div>
-          <div className="border-solid border-2 border-current">
+          <div className="hover:scale-105 transition-transform">
             <YouTubeEmbed
               src="https://www.youtube.com/embed/0MS3OUyOnyo"
               title="Rodeo"
             />
-            <div className="border-solid border-t-2 border-current p-2 text-center">
+            <div className="p-2 text-xl text-center font-outline-2">
               <h2>Rodeo</h2>
             </div>
           </div>
-          <div className="border-solid border-2 border-current">
+          <div className="hover:scale-105 transition-transform">
             <YouTubeEmbed
               src="https://www.youtube.com/embed/mT56hZhmP9E"
               title="The Thunder Rolls"
             />
-            <div className="border-solid border-t-2 border-current p-2 text-center">
+            <div className="p-2 text-xl text-center font-outline-2">
               <h2>The Thunder Rolls</h2>
             </div>
           </div>
